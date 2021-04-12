4f7fd506ec08bb34fadc0b2989ca9ef2c7b63929
Alexander Hernandez <bestrafermich@gmail.com>
2021-04-12 14:22:26 -0700

    Fixed btc abbreviation, and added keys to pair buttons

--- client/src/PairButtons.jsx
+++ client/src/PairButtons.jsx
@@ -1,14 +1,15 @@
 import React, {Component} from 'react'
 
 const PairButtons = (props) => {
-  const pairs = ['xbt','eth','xrp','bch','ada', 'link','dash','eos',
+  const pairs = ['btc','eth','xrp','bch','ada', 'link','dash','eos',
                 'ltc','dot','xtz','trx','usdc','algo','atom','etc',
                 'fil','rep','omg','xlm','xmr','usdt','zec']
   return (
   <div className='buttons'>
     {pairs.map((pair, idx) => {
       return <button className='button is-dark'
-                      onClick={() => props.onClick(pair)}>
+                      onClick={() => props.onClick(pair)}
+                      key={idx}>
         {pair.toUpperCase()}
       </button>}
     )}
