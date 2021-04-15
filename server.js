1bbed7a740b24437906bfa794576b1134f00cd13
Alexander Hernandez <bestrafermich@gmail.com>
2021-04-15 13:10:53 -0700

    a production ready build is now served from the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -132,6 +133,13 @@ app.post('/api/world', (req, res) => {
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
+});
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+// app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build','index.html'))
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
