b9e7b5a299614507e12c52983096aabbad9ed934
Alexander Hernandez <bestrafermich@gmail.com>
2021-04-28 06:09:00 -0700

    replaced axios with got

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 // const { RESTClient } = require('cw-sdk-node')
 const axios = require('axios')
 const utils = require('./utils/utils');
+const got = require('got');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,43 +24,51 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.get('/api/ohlc/:pair', (req, res) => {
-  axios.get(`https://api.cryptowat.ch/markets/kraken/${req.params.pair}usd/ohlc`)
-  .then(res => res.data)
-  .then((candles) => {
-    let results = candles.result
+  (async () => {
+  	try {
+      console.log('request started')
+  		const response = await got(`https://api.cryptowat.ch/markets/kraken/${req.params.pair}usd/ohlc`);
+      let candles = JSON.parse(response.body)
+      let results = candles.result
+      windows = Object.keys(results).map((key, idx) => {
+        let ohlcs = results[key],
+            dates = ohlcs.map(dp => dp[0]),
+            opens = ohlcs.map(dp => dp[1]),
+            highs = ohlcs.map(dp => dp[2]),
+            lows  = ohlcs.map(dp => dp[3]),
+            closes = ohlcs.map(dp => dp[4]),
+            rsi = utils.calculateRsi(dates, closes, 100),
+            psar = utils.calculatePSar(dates, highs, lows, 100),
+            bband = utils.calculateBollingerBand(dates, closes, 100)
+            // ema = utils.calculateEMA(dates, closes, 100),
+            // ichimokuCloud = utils.calculateIchimokuClouds(dates, highs, lows, closes, parseInt(key))
+        console.log('calculations made')
+        return {
+          timeframe: key,
+          dates: dates,
+          prices: ohlcs,
+          rsi: rsi,
+          psar: psar,
+          bband: bband,
+          // ema: ema,
+          // ichimokuCloud: ichimokuCloud,
+        }
+      })
 
-    windows = Object.keys(results).map((key, idx) => {
-      let ohlcs = results[key],
-          dates = ohlcs.map(dp => dp[0]),
-          opens = ohlcs.map(dp => dp[1]),
-          highs = ohlcs.map(dp => dp[2]),
-          lows  = ohlcs.map(dp => dp[3]),
-          closes = ohlcs.map(dp => dp[4]),
-          rsi = utils.calculateRsi(dates, closes, 100),
-          psar = utils.calculatePSar(dates, highs, lows, 100),
-          bband = utils.calculateBollingerBand(dates, closes, 100),
-          ema = utils.calculateEMA(dates, closes, 100),
-          ichimokuCloud = utils.calculateIchimokuClouds(dates, highs, lows, closes, parseInt(key))
-      return {
-        timeframe: key,
-        dates: dates,
-        prices: ohlcs,
-        rsi: rsi,
-        psar: psar,
-        bband: bband,
-        ema: ema,
-        ichimokuCloud: ichimokuCloud,
-      }
-    })
+      windows = windows.map(dp => {
+        return [dp.timeframe, dp]
+      })
 
-    windows = windows.map(dp => {
-      return [dp.timeframe, dp]
-    })
 
-    res.json({
-      windows: Object.fromEntries(windows)
-    })
-  })
+      res.json({
+        windows: Object.fromEntries(windows)
+      })
+  		//=> '<!doctype html> ...'
+  	} catch (error) {
+  		console.log(error.response.body);
+  		//=> 'Internal server error ...'
+  	}
+  })();
 });
 
 app.post('/api/world', (req, res) => {
