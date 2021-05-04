bb40925f0fabccb68e00484160bfee76c3554b68
Alexander Hernandez <bestrafermich@gmail.com>
2021-05-04 14:15:37 -0700

    made body fullheight and uniform color, colored pair buttons

--- client/src/PairButtons.jsx
+++ client/src/PairButtons.jsx
@@ -1,11 +1,11 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 const PairButtons = (props) => {
   const pairs = ['btc','eth','xrp','bch','ada', 'link','dash','eos',
                 'ltc','dot','xtz','trx','usdc','algo','atom','etc',
-                'fil','rep','omg','xlm','xmr','usdt','zec']
+                'fil','rep','omg','xlm','xmr','usdt','zec', 'doge']
   return (
-  <div className='buttons'>
+  <div className='buttons pair-button'>
     {pairs.map((pair, idx) => {
       return <button className='button is-dark'
                       onClick={() => props.onClick(pair)}
