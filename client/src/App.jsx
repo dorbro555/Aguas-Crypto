62d6c0a00ab9f1247e910b4a6bf89387b3428c3c
Alexander Hernandez <bestrafermich@gmail.com>
2021-05-11 13:08:53 -0700

    Added Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import MarketView from './MarketView'
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div className="App">
     <div className='columns has-background-dark'>
       <div className='column is-12'>
+        <NavBar/>
         <MarketView/>
       </div>
     </div>
---
+++ client/src/NavBar.jsx
@@ -0,0 +1,26 @@
+
+function NavBar(props){
+
+  return(
+    <div className='level mb-3'>
+      <div className='level-left ml-1'>
+        <div className='level-item'>
+          <h1 className='title is-2 py-0 has-text-white'>¡Aguas! Crypto</h1>
+        </div>
+      </div>
+      <div className='level-right  mr-1'>
+        <div className='level-item'>
+          <button className='button is-danger'>
+            {
+              props.isAuthenticated
+              ?<a href='http://localhost:5000/logout'>Log out</a>
+              :<a href='http://localhost:5000/login'>Log in</a>
+            }
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
