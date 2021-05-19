fde9573a6a3d6b65ce5bdbc8b8a64e585d81815d
Alexander Hernandez <bestrafermich@gmail.com>
2021-05-18 19:51:29 -0700

    added a sidebar and font-awesome icons support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,33 @@ import NavBar from './NavBar'
 function App() {
   return (
     <div className="App">
-    <div className='columns has-background-dark'>
+      <div className="sidebar">
+        <div className='columns'>
+          <div className='column'>
+            <div className='menu'>
+              <ul className='menu-list'>
+                <a href="#">
+                  <span className='icon'>
+                    <i class="fas fa-list has-text-white"></i>
+                  </span>
+                </a>
+                <a href="#">
+                  <span className='icon'>
+                    <i class="far fa-bell has-text-white"></i>
+                  </span>
+                </a>
+                <a href="#">
+                  <span className='icon'>
+                    <i class="fas fa-retweet has-text-white"></i>
+                  </span>
+                </a>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    <div className='columns has-background-dark ml-6'>
       <div className='column is-12'>
         <NavBar/>
         <MarketView/>
