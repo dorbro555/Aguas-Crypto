5154711f4072692ad7f14d03935d6a646cbc86e0
Alexander Hernandez <bestrafermich@gmail.com>
2021-05-19 16:56:47 -0700

    moved gradient to title bar

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -2,7 +2,7 @@
 function NavBar(props){
 
   return(
-    <div className='level mb-3'>
+    <div className='level mb-3 title-bar'>
       <div className='level-left ml-1'>
         <div className='level-item'>
           <h1 className='title is-2 py-0 has-text-white'>¡Aguas! Crypto</h1>
