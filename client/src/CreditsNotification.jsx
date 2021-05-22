18248f531509284580f0b24db24122f62e7b8b3c
Alexander Hernandez <bestrafermich@gmail.com>
2021-05-22 10:27:55 -0700

    Made entire credit notification clickable

--- client/src/CreditsNotification.jsx
+++ client/src/CreditsNotification.jsx
@@ -7,9 +7,9 @@
      <div>
        {
          props.visible &&
-         <div className='columns'>
-           <div className="notification column is-2 has-background-info-light has-text-info-dark"  style={{position: 'fixed', top: '4%', right: 0}}>
-            <button className="delete" onClick={props.handleClick}></button>
+         <div className='columns is-clickable' onClick={props.handleClick}>
+           <div className="notification column is-2 has-background-info-light has-text-info-dark"  style={{position: 'fixed', top: '4%', right: 0}} >
+            <button className="delete"></button>
               <div>
                 <div>
                   Credits used: {props.allowance.current}
