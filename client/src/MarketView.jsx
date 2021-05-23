7862e037f1870ec73d5552d8d425031c48424c55
Alexander Hernandez <bestrafermich@gmail.com>
2021-05-22 19:55:12 -0700

    clipped bottom of market view

--- client/src/MarketView.jsx
+++ client/src/MarketView.jsx
@@ -24,7 +24,7 @@ class MarketView extends Component {
       data: [], //we must clear the data or the graph wont update
       showWatchlist: false //hide watchlist after clicking an item
     })
-    fetch(`https://www.ahernandez.dev/trade/api/ohlc/${pair}`)
+    fetch(`api/ohlc/${pair}`)
     .then(res => res.json())
     .then(res => {
       this.setState({
@@ -41,12 +41,11 @@ class MarketView extends Component {
   render(){
     let alerts = this.state.data.alerts || []
     return(
-      <div>
-        <div className='columns is-gapless'>
+        <div className='columns is-gapless is-clipped'>
           <div className={this.props.watchListVisible || this.props.alertsListVisible ? 'column is-10' : 'column is-12'}>
             <Charts activePair={this.state.activePair} windows={this.state.data.windows}/>
-            {this.state.data.allowance && <CreditsNotification allowance={this.state.data.allowance} visible={this.state.showCreditsNotification} handleClick={this.toggleCreditsNotification}/>}
           </div>
+          {this.state.data.allowance && <CreditsNotification allowance={this.state.data.allowance} visible={this.state.showCreditsNotification} handleClick={this.toggleCreditsNotification}/>}
           {this.props.watchListVisible &&
             <div className='column is-2'>
               <WatchList onClick={this.setActivePair}/>
@@ -59,12 +58,11 @@ class MarketView extends Component {
             </div>
           }
         </div>
-      </div>
     )
   }
 
   componentDidMount(){
-    fetch(`https://www.ahernandez.dev/trade/api/ohlc/eth`)
+    fetch(`api/ohlc/eth`)
     .then(res => res.json())
 		.then(res => {
       this.setState({
