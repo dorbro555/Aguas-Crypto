a2d8a92584d94775bcfd258d9582f364831f3d29
dorbro555 <dorbro555@proton.me>
2024-06-03 05:30:08 -0700

    added padding to navbar

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar(props){
     <div className='level mb-3 is-mobile has-background-primary'>
       <div className='level-left ml-1'>
         <div className='level-item'>
-          <h1 className='title is-2 py-0 has-text-white is-unselectable'>¡Aguas! Crypto</h1>
+          <h1 className='title is-2 py-2 is-unselectable'>CryptoScout</h1>
         </div>
       </div>
       <div className='level-right  mr-1'>
